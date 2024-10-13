05bce9666d3fafa7fd3c9268f669f63e2dcff178
SALAH UDDIN <155283692+salahuddin24@users.noreply.github.com>
2024-10-13 12:55:58 +0600

    Add files via upload

---
+++ Polymorphism.py
@@ -0,0 +1,14 @@
+# polymorphism bangla meaning বহুরূপতা
+
+# build in Polymorphic function
+print(len("Anisul Islam"))
+print(len([10, 20, 30]))
+
+# user defined polymorphic function
+def add(x, y, z=0) :
+     return  x + y + z
+
+print(add(2,4))
+print(add(2,4,9))
+
+## site : geeksforgeeks.org/polymorphism-in-python/
